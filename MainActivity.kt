76ce0e3c396ad7ab30e818303d660cecbfc09659
ruijpedro <rjpedro@gmail.com>
2026-05-24 23:36:28 +0100

    Delete MainActivity.kt

--- MainActivity.kt
+++
@@ -1,8 +0,0 @@
-<resources>
-    <style name="AppTheme" parent="android:style/Theme.Material.Light.NoActionBar">
-        <item name="android:fontFamily">sans</item>
-        <item name="android:statusBarColor">#BEEBFF</item>
-        <item name="android:navigationBarColor">#BEEBFF</item>
-        <item name="android:windowLightStatusBar">true</item>
-    </style>
-</resources>
